7bd9716f5621252e736d20b94d960abe7ea9e584
Fellipe Barros <fellipe.barros@ztech.net>
2021-11-30 19:48:17 -0300

    fix db env

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,25 +1,34 @@
 import * as sequelize from "sequelize";
+import * as dotenv from "dotenv";
+
 import usersModel from "./models/users";
+
+dotenv.config();
 
 const { Sequelize } = sequelize;
 
 const db = <any>{};
 
-const sequelizeConnection = new Sequelize("dwinfotec", "root", "", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  define: {
-    freezeTableName: true,
-  },
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-  operatorsAliases: null,
-});
+const sequelizeConnection = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: 3306,
+    dialect: "mysql",
+    define: {
+      freezeTableName: true,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    operatorsAliases: null,
+  }
+);
 
 const models = [usersModel];
 
